b32b4ec37230a18722c37321fbeaf712f41d271b
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-05-07 18:19:59 +0600

    Initial commit from Create Next App

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="grid place-items-center min-w-screen min-h-screen text-6xl">
+      <h1>NOORIX.</h1>
+      <p>Where Noor </p>
+
+    </main>
+  );
+}
