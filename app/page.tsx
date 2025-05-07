6cfe915ded33878a5a8c13920c5a11e5a5074678
Md Nifad Uzzaman <nifaduzzaman2005@gmail.com>
2025-05-07 18:36:05 +0600

    Start -nifad

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="grid place-items-center min-w-screen min-h-screen text-6xl">
-      <h1>NOORIX.</h1>
-      <p>Where Noor </p>
+      
+      <div>
+        <h1 className="font-bold text-slate-800">NOORIX</h1>
+        <p className="text-lg font-serif text-slate-500">Where Noor mees Logic.</p>
+      </div>
 
     </main>
   );
